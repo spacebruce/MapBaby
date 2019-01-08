e23471c8cc651faea0de8f6dfe77236284dc705a
bakagamedev <noel.abercrombie@hotmail.com>
2019-01-08 23:43:59 +0000

    Close window and shutdown SDL at end of program

--- MapBaby/MapBaby/Main.cpp
+++ MapBaby/MapBaby/Main.cpp
@@ -28,8 +28,10 @@ int main(int argCount, char * args[])
 	SDL_GLContext gl_context = SDL_GL_CreateContext(window);
 	SDL_GL_SetSwapInterval(1); // Enable vsync
 
-	SDL_Delay(1000);
 
+
+	SDL_GL_DeleteContext(gl_context);
 	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return 0;
 }
