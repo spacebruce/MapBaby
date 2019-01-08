566968a175ec224f27ca6133554962596dd5a960
bakagamedev <noel.abercrombie@hotmail.com>
2019-01-08 23:42:30 +0000

    Start SDL up correctly

--- MapBaby/MapBaby/Main.cpp
+++ MapBaby/MapBaby/Main.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_sdl.h"
 #include "imgui/imgui_impl_opengl2.h"
@@ -8,6 +9,12 @@
 int main(int argCount, char * args[])
 {
 	SDL_Window * window = SDL_CreateWindow("SDL!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 400, SDL_WindowFlags::SDL_WINDOW_ALLOW_HIGHDPI);
+	// Setup SDL
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
+	{
+		printf("Error: %s\n", SDL_GetError());
+		return -1;
+	}
 
 	SDL_Delay(1000);
 
