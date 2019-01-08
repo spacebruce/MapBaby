da55e20e362d35f119671764235c66faadb4b93c
bakagamedev <noel.abercrombie@hotmail.com>
2019-01-08 23:43:24 +0000

    Create OpenGL Context

--- MapBaby/MapBaby/Main.cpp
+++ MapBaby/MapBaby/Main.cpp
@@ -1,4 +1,6 @@
+
 #include <stdio.h>
+
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_sdl.h"
 #include "imgui/imgui_impl_opengl2.h"
@@ -15,9 +17,16 @@ int main(int argCount, char * args[])
 		return -1;
 	}
 	// Setup window
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 	SDL_DisplayMode current;
 	SDL_GetCurrentDisplayMode(0, &current);
 	SDL_Window* window = SDL_CreateWindow("MapBaby", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+	SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+	SDL_GL_SetSwapInterval(1); // Enable vsync
 
 	SDL_Delay(1000);
 
