5aeb5d95a5d429c4f5bfa8a31da1bce31e876060
bakagamedev <noel.abercrombie@hotmail.com>
2019-01-09 23:44:25 +0000

    Use WindowWidth/Height in projection viewport

--- MapBaby/MapBaby/MapEditor.cpp
+++ MapBaby/MapBaby/MapEditor.cpp
@@ -11,11 +11,11 @@ MapEditor::~MapEditor()
 void MapEditor::render(const int WindowWidth, const int WindowHeight)
 {
 	//Set the viewport
-	glViewport( 0.f, 0.f, static_cast<float>(WindowWidth), static_cast<float>(WindowHeight));
-	glMatrixMode( GL_PROJECTION ); 
+	glViewport( 0, 0, WindowWidth, WindowHeight);
+	glMatrixMode(GL_PROJECTION); 
 	glLoadIdentity(); 
-	glOrtho( 0.0, 1280, 720, 0.0, 1.0, -1.0 ); 
-	glMatrixMode( GL_MODELVIEW ); 
+	glOrtho( 0.0, WindowWidth, WindowHeight, 0.0, 1.0, -1.0 );
+	glMatrixMode(GL_MODELVIEW); 
 	glLoadIdentity(); 
 
 	glTranslatef(0.0f, 0.0f, 0.0f);
