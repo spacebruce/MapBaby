397de75bc850040d480996d7fb9e8f3433d74af4
bakagamedev <noel.abercrombie@hotmail.com>
2019-01-09 15:22:26 +0000

    Render test rectangle in map editor

--- MapBaby/MapBaby/MapEditor.cpp
+++ MapBaby/MapBaby/MapEditor.cpp
@@ -7,3 +7,9 @@ MapEditor::MapEditor()
 MapEditor::~MapEditor()
 {
 }
+
+void MapEditor::render()
+{
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glRectf(-0.75f, 0.75f, 0.75f, -0.75f);
+}
--- MapBaby/MapBaby/MapEditor.h
+++ MapBaby/MapBaby/MapEditor.h
@@ -8,4 +8,6 @@ class MapEditor
 public:
 	MapEditor();
 	~MapEditor();
+
+	void render();
 };
