c7318f06acf2b481d0c9aaa1a8a2bd02d491c545
bakagamedev <noel.abercrombie@hotmail.com>
2019-01-09 14:56:39 +0000

    Remove unneeded headers from main

--- MapBaby/MapBaby/Main.cpp
+++ MapBaby/MapBaby/Main.cpp
@@ -1,13 +1,3 @@
-
-#include <stdio.h>
-
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_sdl.h"
-#include "imgui/imgui_impl_opengl2.h"
-
-#include <SDL.h>
-#include <SDL/SDL_opengl.h>
-
 #include "UserInterface.h"
 
 int main(int argCount, char * args[])
