3cf48ad1a1c04d08361b250f8d21eccd75439317
bakagamedev <noel.abercrombie@hotmail.com>
2019-01-10 23:45:13 +0000

    Draw placeholder tile grid

--- MapBaby/MapBaby/MapEditor.cpp
+++ MapBaby/MapBaby/MapEditor.cpp
@@ -30,7 +30,21 @@ void MapEditor::render(const int WindowWidth, const int WindowHeight)
 		return;
 
 	//Draw stuff
-	Drawing::drawRectangleOutline(0, 0, 256, 256);// map->Width, map->Height);
+	const int Width = 12;
+	const int Height = 8;
+	const int TileSize = 16;
+	const int WidthReal = Width * TileSize;
+	const int HeightReal = Height * TileSize;
+
+	Drawing::drawRectangleOutline(0, 0, WidthReal, HeightReal);
+	for (int i = 0; i < Width; ++i)
+	{
+		Drawing::drawLine(i * TileSize, 0.0f, i * TileSize, HeightReal);
+	}
+	for (int i = 0; i < Height; ++i)
+	{
+		Drawing::drawLine(0.0f, i * TileSize, WidthReal, i * TileSize);
+	}
 }
 
 void MapEditor::changeMap(Map * map)
