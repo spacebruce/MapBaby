f6bbe86355acd013cde82e546759f381eb512290
bakagamedev <noel.abercrombie@hotmail.com>
2019-01-10 00:28:36 +0000

    Create MapManager
    for the management of maps

---
+++ MapBaby/MapBaby/MapManager.cpp
@@ -0,0 +1,21 @@
+#include "MapManager.h"
+
+MapManager::MapManager()
+{
+}
+
+MapManager::~MapManager()
+{
+}
+
+Map * MapManager::getMap(std::size_t index)
+{
+	if((index < 0) || (index >> getCount()))
+		return nullptr;
+	return &mapPool[index];
+}
+
+std::size_t MapManager::getCount()
+{
+	return mapPool.size();
+}
---
+++ MapBaby/MapBaby/MapManager.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <vector>
+#include "Map.h"
+
+class MapManager
+{
+private:
+	std::vector<Map> mapPool;
+public:
+	MapManager();
+	~MapManager();
+
+	Map * getMap(std::size_t index);
+	std::size_t getCount();
+};
