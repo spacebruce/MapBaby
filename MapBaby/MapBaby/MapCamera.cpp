554cebcd4b952b2b82785a7e2ee923f26b567d89
bakagamedev <noel.abercrombie@hotmail.com>
2019-01-11 01:24:43 +0000

    Calculate world mouse coords

--- MapBaby/MapBaby/MapCamera.cpp
+++ MapBaby/MapBaby/MapCamera.cpp
@@ -18,3 +18,19 @@ void MapCamera::updateOrtho(const int WindowWidth, const int WindowHeight)
 	bottom = y + (windowHeight / 2) / zoom;
 }
 
+void MapCamera::updateMouse(const int WindowWidth, const int WindowHeight)
+{
+	//requires projection to be up to date!
+	float mouseX, mouseY;
+	{
+		int mx, my;
+		SDL_GetMouseState(&mx, &my);
+		mouseX = static_cast<float>(mx);
+		mouseY = static_cast<float>(my);
+	}
+	float mouseXnormal = Numbers::mapValue(mouseX, 0.0f, static_cast<float>(WindowWidth), 0.0f, 1.0f);
+	float mouseYnormal = Numbers::mapValue(mouseY, 0.0f, static_cast<float>(WindowHeight), 0.0f, 1.0f);
+
+	mouseX = Numbers::mapValue(mouseXnormal, 0.0f, 1.0f, left, right);
+	mouseY = Numbers::mapValue(mouseYnormal, 0.0f, 1.0f, top, bottom);
+}
