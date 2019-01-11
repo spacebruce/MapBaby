0c80fb99f0d4082f3ed60e0b0e7eeb068a76a0b7
bakagamedev <noel.abercrombie@hotmail.com>
2019-01-11 14:24:49 +0000

    Add Rectangle and Point to Numbers

--- MapBaby/MapBaby/MathsHelpers.h
+++ MapBaby/MapBaby/MathsHelpers.h
@@ -2,6 +2,30 @@
 
 namespace Numbers
 {
+	template< typename T>
+	struct Rectangle
+	{
+		T left, right, top, bottom;
+		Rectangle(T left, T right, T top, T bottom)
+		{
+			this->left = left;
+			this->right = right;
+			this->top = top;
+			this->bottom = bottom;
+		}
+	};
+
+	template< typename T>
+	struct Point
+	{
+		T x, y;
+		Point(T x, T y)
+		{
+			this->x = x;
+			this->y = y;
+		}
+	};
+
 	template <typename T>
 	T mapValue(const T value, const T fromLow, const T fromHigh, const T toLow, const T toHigh)
 	{
