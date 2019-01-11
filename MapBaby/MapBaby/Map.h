3fbe5371b99a3296a4fa789a4f7b4f2ee6e95e8d
bakagamedev <noel.abercrombie@hotmail.com>
2019-01-11 13:22:42 +0000

    Add constructor and setSize function to Map

--- MapBaby/MapBaby/Map.h
+++ MapBaby/MapBaby/Map.h
@@ -5,4 +5,15 @@ struct Map
 	int Width = 12;
 	int Height = 12;
 	int TileSize = 16;
+
+	Map() = default;
+	Map(int width, int height)
+	{
+		this->setSize(width, height);
+	}
+	void setSize(int width, int height)
+	{
+		this->Width = width;
+		this->Height = height;
+	}
 };
