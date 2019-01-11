d1fd21a70553bd56b4e248994e58801a8f84f3b1
bakagamedev <noel.abercrombie@hotmail.com>
2019-01-11 12:31:25 +0000

    Fix view coord and mouse position text

--- MapBaby/MapBaby/UserInterface.cpp
+++ MapBaby/MapBaby/UserInterface.cpp
@@ -140,12 +140,12 @@ void UserInterface::updateWindows()
 	{
 		ImGui::Begin("View", &ShowViewWindow, ImGuiWindowFlags_AlwaysAutoResize);
 
-		ImGui::Text("mouse x : %f", &(mapEditor->camera.mouseX));
-		ImGui::Text("mouse y : %f", &(mapEditor->camera.mouseY));
-		ImGui::Text("cam l%f", &(mapEditor->camera.left));
-		ImGui::Text("cam r%f", &(mapEditor->camera.right));
-		ImGui::Text("cam t%f", &(mapEditor->camera.top));
-		ImGui::Text("cam b%f", &(mapEditor->camera.bottom));
+		ImGui::Text("mouse x : %f", mapEditor->camera.mouseY);
+		ImGui::Text("mouse y : %f", mapEditor->camera.mouseY);
+		ImGui::Text("cam l %f", mapEditor->camera.left);
+		ImGui::Text("cam r %f", mapEditor->camera.right);
+		ImGui::Text("cam t %f", mapEditor->camera.top);
+		ImGui::Text("cam b %f", mapEditor->camera.bottom);
 		//Position
 		if (ImGui::CollapsingHeader("Position"))
 		{
