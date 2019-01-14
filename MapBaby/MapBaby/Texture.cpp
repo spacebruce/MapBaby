e0daecc9211f5f83155e6b0d3d290b9006971b20
bakagamedev <noel.abercrombie@hotmail.com>
2019-01-14 12:04:58 +0000

    Update Texture

--- MapBaby/MapBaby/Texture.cpp
+++ MapBaby/MapBaby/Texture.cpp
@@ -6,6 +6,41 @@ Texture::Texture()
 
 Texture::~Texture()
 {
+	freeTexture();
+}
+
+void Texture::freeTexture()
+{
+	if (this->isLoaded())
+	{
+		glDeleteTextures(1, &this->TextureID);
+		this->TextureID = 0;
+	}
+	this->Width = 0;
+	this->Height = 0;
+}
+
+void Texture::createFromArray(GLuint Width, GLuint Height, GLuint *pixels)
+{
+	this->freeTexture();
+
+	this->Width = Width;
+	this->Height = Height;
+
+	glGenTextures(1, &this->TextureID);
+	glBindTexture( GL_TEXTURE_2D, this->TextureID);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, Width, Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+	glBindTexture(GL_TEXTURE_2D, NULL);
+}
+
+GLuint Texture::getTexture() const
+{
+	return this->TextureID;
 }
 
 int Texture::getWidth() const
@@ -16,4 +51,9 @@ int Texture::getWidth() const
 int Texture::getHeight() const
 {
 	return this->Height;
+}
+
+bool Texture::isLoaded() const
+{
+	return (this->TextureID != 0);
 }
--- MapBaby/MapBaby/Texture.h
+++ MapBaby/MapBaby/Texture.h
@@ -1,14 +1,26 @@
 #pragma once
+#include <vector>
+#include <array>
+
+#include <SDL.h>
+#include <SDL/SDL_opengl.h>
 
 class Texture
 {
 private:
-	int Width, Height;
+	GLuint TextureID = 0;
+	int Width = 0;
+	int Height = 0;
 public:
 	Texture();
 	~Texture();
 
+	void createFromArray(GLuint Width, GLuint Height, GLuint * pixels);
+	void freeTexture();
+
+	GLuint getTexture() const;
 	int getWidth() const;
 	int getHeight() const;
+	bool isLoaded() const;
 };
 
