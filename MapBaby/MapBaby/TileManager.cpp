96accdfef928f0735640928317c441b80ec878bd
bakagamedev <noel.abercrombie@hotmail.com>
2019-01-14 12:17:46 +0000

    Create dummy tile in TileManager

--- MapBaby/MapBaby/TileManager.cpp
+++ MapBaby/MapBaby/TileManager.cpp
@@ -7,3 +7,35 @@ TileManager::TileManager()
 TileManager::~TileManager()
 {
 }
+
+void TileManager::createTile()
+{
+	Tile newTile;
+	Texture * tex = newTile.getTexture();
+
+	//dummy texture
+	constexpr static int width = 16;
+	constexpr static int height = 16;
+	GLuint pixels[width * height];
+	for (int i = 0; i < (width * height); ++i)
+	{
+		GLubyte* colour = (GLubyte*)&pixels[i];
+		if (i & 4)
+		{
+			colour[0] = 0xff;
+			colour[1] = 0xff;
+			colour[2] = 0xff;
+			colour[3] = 0xff;
+		}
+		else
+		{
+			colour[0] = 0x00;
+			colour[1] = 0x00;
+			colour[2] = 0x00;
+			colour[3] = 0xff;
+		}
+	}
+	tex->createFromArray(width, height, pixels);
+
+	tilePool.emplace_back(newTile);
+}
