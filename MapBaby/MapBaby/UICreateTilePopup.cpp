fb53759140916579ab86b329db28cf355c097c47
spacebruce <noel.abercrombie@hotmail.com>
2019-01-19 23:38:42 +0000

    Add basic Tile popup functionality

--- MapBaby/MapBaby/UICreateTilePopup.cpp
+++ MapBaby/MapBaby/UICreateTilePopup.cpp
@@ -14,16 +14,40 @@ UICreateTilePopup::~UICreateTilePopup()
 
 void UICreateTilePopup::reset()
 {
-
+	Width = 16;
+	Height = 16;
+	LockSize = true;
 }
 
 void UICreateTilePopup::updateContents()
 {
 	if (ImGui::BeginPopupModal(this->identifier, nullptr, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoMove))
 	{
-		if (ImGui::Button("make"))
+		ImGui::Button("Load from file");
+		ImGui::Separator();
+
+		ImGui::BeginChild("thing", ImVec2(300, 140));
+
+		ImGui::Columns(2, nullptr, false);
+		ImGui::Image(0, ImVec2(128, 128));
+
+		ImGui::NextColumn();
+
+		ImGui::Checkbox("Lock size", &LockSize);
+		ImGui::InputScalar("Width", ImGuiDataType_U32, &Width);
+		ImGui::InputScalar("Height", ImGuiDataType_U32, &Height);
+
+		ImGui::EndChild();
+
+		ImGui::Separator();
+		if (ImGui::Button("create"))
 		{
 			tileManager->createTile(paletteManager->getCurrentPalette());
+			ImGui::CloseCurrentPopup();
+		}
+		ImGui::SameLine();
+		if (ImGui::Button("close"))
+		{
 			ImGui::CloseCurrentPopup();
 		}
 		ImGui::EndPopup();
--- MapBaby/MapBaby/UICreateTilePopup.h
+++ MapBaby/MapBaby/UICreateTilePopup.h
@@ -6,10 +6,13 @@ class UICreateTilePopup : public UIPopupBase
 protected:
 	void updateContents();
 	void reset();
+private:
+	int Width;
+	int Height;
+	bool LockSize;
 public:
 	UICreateTilePopup();
 	UICreateTilePopup(MapManager * mapManager, PaletteManager * paletteManager, TileManager * tileManager, MapEditor * mapEditor);
 	~UICreateTilePopup();
 
 };
-
