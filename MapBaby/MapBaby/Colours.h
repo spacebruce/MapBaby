b42b4cb2f303405fa1b1823e156db3181a67f59c
bakagamedev <noel.abercrombie@hotmail.com>
2019-01-19 01:04:11 +0000

    Add GLuint casts to ColourRGB and ColourRGBA

--- MapBaby/MapBaby/Colours.h
+++ MapBaby/MapBaby/Colours.h
@@ -10,6 +10,11 @@ struct ColourRGB
 
 	ColourRGB() = default;
 	ColourRGB(GLubyte r, GLubyte g, GLubyte b) : r(r), g(g), b(b) {};
+
+	explicit operator GLuint() const
+	{
+		return static_cast<GLuint>((0xFF << 24) | (b << 16) | (g << 8) | (r << 0));
+	}
 };
 
 struct ColourRGBA
@@ -21,4 +26,9 @@ struct ColourRGBA
 
 	ColourRGBA() = default;
 	ColourRGBA(GLubyte r, GLubyte g, GLubyte b, GLubyte a) : r(r), g(g), b(b), a(a) {};
+
+	explicit operator GLuint() const
+	{
+		return static_cast<GLuint>((a << 24) | (b << 16) | (g << 8) | (r << 0));
+	}
 };
