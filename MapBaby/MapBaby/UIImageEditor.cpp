6cb82fbacbaeb0a5df7877764035714125b67abf
spacebruce <noel.abercrombie@hotmail.com>
2019-01-26 14:14:31 +0000

    Draw tile preview in editor

--- MapBaby/MapBaby/UIImageEditor.cpp
+++ MapBaby/MapBaby/UIImageEditor.cpp
@@ -18,5 +18,9 @@ void UIImageEditor::updateContents()
 	ImGui::Begin("Image editor", &visible);
 	ImGui::Text("Tile ID %i", tile.get()->getID());
 	ImGui::Text("Texture %i", tile.get()->texture.get());
+
+	ImGui::Separator();
+
+	ImGui::Image((void*)tile.get()->texture.get(), ImVec2(256, 256));
 	ImGui::End();
 }
