7056cf5243aa004ae418b440bbdf1d786663bd60
spacebruce <noel.abercrombie@hotmail.com>
2019-01-26 12:19:28 +0000

    Block out ImageEditor class

--- MapBaby/MapBaby/UIImageEditor.cpp
+++ MapBaby/MapBaby/UIImageEditor.cpp
@@ -1,9 +1,20 @@
 #include "UIImageEditor.h"
 
+UIImageEditor::UIImageEditor(TileManager::SharedTile* tile)
+{
+	this->tile = tile;
+}
+
 UIImageEditor::UIImageEditor()
 {
 }
 
 UIImageEditor::~UIImageEditor()
 {
+}
+
+void UIImageEditor::updateContents()
+{
+	ImGui::Begin("Image editor", &visible);
+	ImGui::End();
 }
--- MapBaby/MapBaby/UIImageEditor.h
+++ MapBaby/MapBaby/UIImageEditor.h
@@ -1,10 +1,17 @@
 #pragma once
+#include "Include/Imgui/imgui.h"
 #include "UIWindowBase.h"
+#include "TileManager.h"
+#include "TileType.h"
 
 class UIImageEditor : public UIWindowBase
 {
+private:
+	TileManager::SharedTile * tile;
+
+	void updateContents();
 public:
+	UIImageEditor(TileManager::SharedTile* Tile);
 	UIImageEditor();
 	~UIImageEditor();
 };
-
